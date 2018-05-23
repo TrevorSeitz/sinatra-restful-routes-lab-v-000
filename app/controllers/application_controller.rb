7a1a8a90a3690f85a4cbae3fc956b2a0846bcd77
Trevor Seitz <trevinla@gmail.com>
2018-05-23 14:50:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,11 +43,11 @@ class ApplicationController < Sinatra::Base
     # @recipe = Recipe.find(params[:id])
     @recipe = Recipe.find(params[:id])
 
-    # @recipe.update(params[:id], name => params[:name], ingredients => params[:ingredients], cook_time => params[:cook_time])
-   @recipe.name = params[:name]
-   @recipe.ingredients = params[:ingredients]
-   @recipe.cook_time = params[:cook_time]
-   @recipe.save
+    @recipe.update(params[:id], name => params[:name], ingredients => params[:ingredients], cook_time => params[:cook_time])
+  #  @recipe.name = params[:name]
+  #  @recipe.ingredients = params[:ingredients]
+  #  @recipe.cook_time = params[:cook_time]
+  #  @recipe.save
 
     erb :show
     # redirect "/recipes/#{@recipe.id}"
