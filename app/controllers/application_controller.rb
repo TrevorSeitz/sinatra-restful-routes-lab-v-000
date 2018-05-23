643a4355408cc3fe690ba5e5ef8cec6e4ec452da
Trevor Seitz <trevinla@gmail.com>
2018-05-23 14:05:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
-
+binding.pry
     @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
 
     erb :show
